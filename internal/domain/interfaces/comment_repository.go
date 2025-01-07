f309091371e4f53eb3fefe5aed7939ca6d0dc9eb
rayfiyo <rayfiyorex@gmail.com>
2025-01-07 12:01:11 +0900

    feat: 🍻 domain/interfaces が間違っていた
    ✅ Closes: #1

--- internal/domain/interfaces/comment_repository.go
+++ internal/domain/interfaces/comment_repository.go
@@ -1,35 +1,21 @@
 package interfaces
 
 import (
-	"context"
-
 	"github.com/polupolu-dev/polupolu-backend/internal/domain/models"
 )
 
 // コメントデータソースと対話するメソッドを定義
 type CommentRepository interface {
-	// ニュースへのコメント一覧取得 (MVP)
-	// 仕様: `news_id` からコメント構造体の配列を取得する
-	GetByNewsID(ctx context.Context, newsID string) ([]models.Comment, error)
+	// 作成
+	Create(comment models.Comment) (*models.Comment, error)
 
-	// 特定コメント取得 (MVP)
-	// 仕様: `comment_id` からコメント構造体を取得する
-	GetByCommentID(ctx context.Context, commentID string) (*models.Comment, error)
-
-	// 特定ユーザーのコメント一覧取得 (MVP)
-	// 仕様: `user_id` からコメント構造体の配列を取得する
-	GetByUserID(ctx context.Context, userID string) ([]models.Comment, error)
-
-	// ニュースへのコメント作成 (MVP)
-	// コメントへの返信作成 (MVP)
-	// 仕様: コメント構造体からコメントを作成し，コメント構造体を返す
-	Create(ctx context.Context, comment models.Comment) (*models.Comment, error)
-
-	// 削除
-	// 仕様: `comment_id` からコメントを削除する
-	Delete(ctx context.Context, commentID string) error
+	// 探索（読み込み）
+	Find(id string) (*models.Comment, error)
+	FindList(id string) ([]models.Comment, error)
 
 	// 更新
-	// 仕様: コメント構造体からからコメントを更新し，コメント構造体を返す
-	Update(ctx context.Context, comment models.Comment) (*models.Comment, error)
+	Update(comment models.Comment) (*models.Comment, error)
+
+	// 削除
+	Delete(id string) error
 }
--- internal/domain/interfaces/user_repository.go
+++ internal/domain/interfaces/user_repository.go
@@ -1,25 +1,19 @@
 package interfaces
 
 import (
-	"context"
-
 	"github.com/polupolu-dev/polupolu-backend/internal/domain/models"
 )
 
 type UserRepository interface {
-	// 取得 (MVP)
-	// 仕様: `user_id` からユーザー構造体を取得
-	Get(context context.Context, userID string) (*models.User, error)
+	// 作成
+	Create(user models.User) (*models.User, error)
 
-	// 作成 (MVP)
-	// 仕様: ユーザー構造体からユーザーを作成し，作成したユーザー構造体を返す
-	Create(context context.Context, user models.User) (*models.User, error)
+	// 探索（読み込み）
+	Find(id string) (*models.User, error)
+
+	// 更新
+	Update(user models.User) (*models.User, error)
 
 	// 削除
-	// 仕様: `user_id` からユーザーを削除する
-	Delete(context context.Context, userID string) error
-
-	// 仕様: ユーザー構造体からユーザーを更新し，更新したユーザー構造体を返す
-	// 名前: updateUser
-	Update(context context.Context, user models.User) (*models.User, error)
+	Delete(id string) error
 }
