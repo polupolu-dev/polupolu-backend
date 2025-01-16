49cb0bb865b9fd92a1de0d1fe9363979399b3445
rayfiyo <rayfiyorex@gmail.com>
2025-01-17 05:27:48 +0900

    fix: 🐛 構造体分離に伴う修正

--- internal/domain/models/comment.go
+++ internal/domain/models/comment.go
@@ -5,15 +5,11 @@ import (
 )
 
 type Comment struct {
-	ID             string    `json:"id"`
-	ReplyToID      string    `json:"reply_to_id"`
-	RepliedIDs     []string  `json:"replied_ids"`
-	UserID         string    `json:"user_id"`
-	Content        string    `json:"content"`
-	CreatedAt      time.Time `json:"created_at"`
-	FeedbackScores struct {
-		Empathy  uint32 `json:"empathy"`
-		Insight  uint32 `json:"insight"`
-		Mediocre uint32 `json:"mediocre"`
-	} `json:"feedback_scores"`
+	ID             string         `json:"id"`
+	ReplyToID      string         `json:"reply_to_id"`
+	RepliedIDs     []string       `json:"replied_ids"`
+	UserID         string         `json:"user_id"`
+	Content        string         `json:"content"`
+	CreatedAt      time.Time      `json:"created_at"`
+	FeedbackScores FeedbackScores `json:"feedback_scores"`
 }
