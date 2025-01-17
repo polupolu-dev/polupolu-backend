4e86647be82f7c128a4d0d8d764e1dcb165dd381
rayfiyo <rayfiyorex@gmail.com>
2025-01-17 22:38:12 +0900

    feat: 🍻 main.go

---
+++ cmd/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+	"net/http"
+
+	"github.com/polupolu-dev/polupolu-backend/internal/adapter/handlers"
+	"github.com/polupolu-dev/polupolu-backend/internal/adapter/router"
+	"github.com/polupolu-dev/polupolu-backend/internal/domain/interfaces"
+	"github.com/polupolu-dev/polupolu-backend/internal/infrastructure/postgres"
+	"github.com/polupolu-dev/polupolu-backend/internal/usecase"
+)
+
+func main() {
+	dbConn, err := sql.Open("postgres", "user=your_user dbname=your_db sslmode=disable")
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+	defer dbConn.Close()
+
+	commentRepo := postgres.NewCommentRepository(dbConn)
+	newsRepo := postgres.NewNewsRepository(dbConn)
+	userRepo := postgres.NewUserRepository(dbConn)
+	var llmService interfaces.LLMService
+
+	commentUC := usecase.NewCommentUsecase(commentRepo, newsRepo, userRepo, llmService)
+	newsUC := usecase.NewNewsUsecase(newsRepo, llmService)
+	userUC := usecase.NewUserUsecase(userRepo)
+
+	commentHandler := handlers.NewCommentHandler(commentUC)
+	newsHandler := handlers.NewNewsHandler(newsUC)
+	userHandler := handlers.NewUserHandler(userUC)
+
+	r := router.NewRouter(commentHandler, newsHandler, userHandler)
+
+	log.Println("Server started at :8080")
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
