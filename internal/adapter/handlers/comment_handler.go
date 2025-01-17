cab0693e178d7dc073910e2fb043a53f28833ac0
rayfiyo <rayfiyorex@gmail.com>
2025-01-18 02:50:23 +0900

    feat: 🍻 string -> uuid の修正

--- internal/adapter/handlers/comment_handler.go
+++ internal/adapter/handlers/comment_handler.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"net/http"
 
+	"github.com/google/uuid"
 	"github.com/gorilla/mux"
 	"github.com/polupolu-dev/polupolu-backend/internal/domain/models"
 	"github.com/polupolu-dev/polupolu-backend/internal/usecase"
@@ -19,76 +20,105 @@ func NewCommentHandler(cu *usecase.CommentUsecase) *CommentHandler {
 
 func (h *CommentHandler) GetCommentsForNews(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
-	newsID := vars["news_id"]
+	newsID, err := uuid.Parse(vars["news_id"])
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
 	comments, err := h.commentUseCase.GetCommentsForNews(r.Context(), newsID)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(comments)
 }
 
 func (h *CommentHandler) GetComment(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
-	commentID := vars["comment_id"]
+	commentID, err := uuid.Parse(vars["comment_id"])
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
 	comment, err := h.commentUseCase.GetComment(r.Context(), commentID)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusNotFound)
 		return
 	}
+
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(comment)
 }
 
 func (h *CommentHandler) GetUserComments(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
-	userID := vars["user_id"]
+	userID, err := uuid.Parse(vars["user_id"])
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
 	user, err := h.commentUseCase.GetUserComments(r.Context(), userID)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusNotFound)
 		return
 	}
+
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(user)
 }
 
 func (h *CommentHandler) CreateComment(w http.ResponseWriter, r *http.Request) {
 	var comment models.Comment
+
 	if err := json.NewDecoder(r.Body).Decode(&comment); err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
+
 	if err := h.commentUseCase.CreateComment(r.Context(), &comment); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	w.Header().Set("Location", "/api/v1/comments/"+comment.ID)
+
+	w.Header().Set("Location", "/api/v1/comments/"+comment.ID.String())
 	w.WriteHeader(http.StatusCreated)
 }
 
 func (h *CommentHandler) CreateReply(w http.ResponseWriter, r *http.Request) {
 	var comment models.Comment
+
 	if err := json.NewDecoder(r.Body).Decode(&comment); err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
+
 	if err := h.commentUseCase.CreateReply(r.Context(), &comment); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	w.Header().Set("Location", "/api/v1/comments/"+comment.ID+"/replies")
+
+	w.Header().Set("Location", "/api/v1/comments/"+comment.ID.String()+"/replies")
 	w.WriteHeader(http.StatusCreated)
 }
 
 func (h *CommentHandler) DeleteComment(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
-	commentID := vars["comment_id"]
-	err := h.commentUseCase.DeleteComment(r.Context(), commentID)
+	commentID, err := uuid.Parse(vars["comment_id"])
 	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if err := h.commentUseCase.DeleteComment(r.Context(), commentID); err != nil {
 		http.Error(w, err.Error(), http.StatusNotFound)
 		return
 	}
+
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusNoContent)
 }
@@ -99,10 +129,12 @@ func (h *CommentHandler) UpdateComment(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
+
 	if err := h.commentUseCase.UpdateComment(r.Context(), &comment); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	w.Header().Set("Location", "/api/v1/comments/"+comment.ID)
+
+	w.Header().Set("Location", "/api/v1/comments/"+comment.ID.String())
 	w.WriteHeader(http.StatusCreated)
 }
--- internal/adapter/handlers/user_handler.go
+++ internal/adapter/handlers/user_handler.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"net/http"
 
+	"github.com/google/uuid"
 	"github.com/gorilla/mux"
 	"github.com/polupolu-dev/polupolu-backend/internal/domain/models"
 	"github.com/polupolu-dev/polupolu-backend/internal/usecase"
@@ -19,12 +20,18 @@ func NewUserHandler(uu *usecase.UserUsecase) *UserHandler {
 
 func (h *UserHandler) GetUser(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
-	userID := vars["user_id"]
+	userID, err := uuid.Parse(vars["user_id"])
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
 	user, err := h.userUseCase.GetUser(r.Context(), userID)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusNotFound)
 		return
 	}
+
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(user)
 }
@@ -35,22 +42,29 @@ func (h *UserHandler) CreateUser(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
+
 	if err := h.userUseCase.CreateUser(r.Context(), &user); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	w.Header().Set("Location", "/api/v1/user/"+user.ID)
+
+	w.Header().Set("Location", "/api/v1/user/"+user.ID.String())
 	w.WriteHeader(http.StatusCreated)
 }
 
 func (h *UserHandler) DeleteUser(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
-	userID := vars["user_id"]
-	err := h.userUseCase.DeleteUser(r.Context(), userID)
+	userID, err := uuid.Parse(vars["user_id"])
 	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if err := h.userUseCase.DeleteUser(r.Context(), userID); err != nil {
 		http.Error(w, err.Error(), http.StatusNotFound)
 		return
 	}
+
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusNoContent)
 }
@@ -61,10 +75,12 @@ func (h *UserHandler) UpdateUser(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
+
 	if err := h.userUseCase.UpdateUser(r.Context(), &user); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	w.Header().Set("Location", "/api/v1/user/"+user.ID)
+
+	w.Header().Set("Location", "/api/v1/user/"+user.ID.String())
 	w.WriteHeader(http.StatusCreated)
 }
